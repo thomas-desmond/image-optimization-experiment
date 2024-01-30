e1151d25a5b95373adf6b65bb14d062dfa671768
Thomas Desmond <24610108+thomas-desmond@users.noreply.github.com>
2024-01-30 13:51:45 -0800

    compare jss to next image optimization

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,85 @@
+import { NextImage } from '@sitecore-jss/sitecore-jss-nextjs';
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useEffect, useState } from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
+let backgroundImage = {
+  "value": {
+    "src": "https://edge.sitecorecloud.io/sitecoresaa94c3-xmcloudintr2ef7-production-9f57/media/Project/Sugcon/shared/man-with-phone-and-umbrella.jpg?h=392&iar=0&w=707",
+    "alt": "",
+    "width": "707",
+    "height": "392"
+  }
+}
 
-export default function Home() {
+export default function Page() {
+
+  const [jssOptimizedSizeInKb, setJssOptimizedSizeInKb] = useState(0);
+  const [nextOptimizedSizeInKb, setNextOptimizedSizeInKb] = useState(0);
+
+  useEffect(() => {
+    const jssOptimized = document.getElementById('jss-image') as HTMLImageElement;
+    getFileSize(jssOptimized.src)
+      .then(sizeInfo => {
+        console.log(`File size:${sizeInfo.kilobytes} KB`);
+        setJssOptimizedSizeInKb(sizeInfo.kilobytes);
+      });
+
+    const nextOptimized = document.getElementById('next-image') as HTMLImageElement;
+    getFileSize(nextOptimized.src)
+      .then(sizeInfo => {
+        console.log(`File size:${sizeInfo.kilobytes} KB`);
+        setNextOptimizedSizeInKb(sizeInfo.kilobytes);
+      });
+  }, []);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main>
+      <div>
+        <p className="text-2xl">Original Size: 87.35 kb</p>
+        <p className="text-2xl">JSS Optimized Size: {jssOptimizedSizeInKb.toFixed(2)} kb</p>
+        <p className="text-2xl">Vercel Optimized Size: {nextOptimizedSizeInKb.toFixed(2)} kb</p>
       </div>
+      <NextImage
+        id="jss-image"
+        field={backgroundImage}
+        alt="man"
+        unoptimized={false}
+        width={707}
+        height={392}
+      />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <Image id="next-image" src="/man.jpg" alt="man" height={392} width={707} />
+      <p>
+        Photo by <a href="https://unsplash.com/@jameswiseman?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">James Wiseman</a> on <a href="https://unsplash.com/photos/a-computer-screen-with-a-program-running-on-it-imgCpfIMoRw?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+      </p>
     </main>
   )
+}
+
+function getFileSize(url: string): Promise<SizeInfo> {
+  console.log(url);
+  return fetch(url)
+    .then(response => {
+      const contentLengthHeader = response.headers.get('Content-Length');
+      if (contentLengthHeader) {
+        const fileSizeInBytes = parseInt(contentLengthHeader, 10);
+        const fileSizeInKB = fileSizeInBytes / 1024;
+
+        return {
+          bytes: fileSizeInBytes,
+          kilobytes: fileSizeInKB,
+        };
+      } else {
+        return Promise.reject('Content-Length header not found in the response.');
+      }
+    })
+    .catch(error => {
+      console.error('Error fetching image:', error);
+      throw error;
+    });
+}
+
+interface SizeInfo {
+  bytes: number;
+  kilobytes: number;
 }
